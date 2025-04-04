1c48d1861bef3fbc61b492d5d90ccfef009d3850
Artem_Teplygin <teplygin07@mail.ru>
2025-04-04 07:41:02 +0300

    start create structure project

---
+++ counter/src/app/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Main from "./Main.tsx";
+
+function App() {
+
+    return (
+        <>
+            <Main/>
+        </>
+    )
+}
+
+export default App
---
+++ counter/src/app/Main.tsx
@@ -0,0 +1,9 @@
+export const Main = () => {
+    return (
+        <div>
+            <div>COUNTER</div>
+        </div>
+    );
+};
+
+export default Main;
