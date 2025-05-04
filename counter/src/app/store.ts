51919393aedc3ffd90adcfc8d42bec0614a7db0a
Artem_Teplygin <teplygin07@mail.ru>
2025-05-04 15:01:01 +0300

    add local storage

--- counter/src/app/store.ts
+++ counter/src/app/store.ts
@@ -1,21 +1,22 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit"
 import {counterReducer} from "../features/counter/model/counter-reducer.ts";
+import {loadState, saveState} from "./utils/loacalStorage-utils.ts";
 
 // объединение reducer'ов с помощью combineReducers
 const rootReducer = combineReducers({
     counter: counterReducer,
 })
 
-// создание store
 export const store = configureStore({
     reducer: rootReducer,
+    preloadedState: loadState(),
+})
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter
+    })
 })
 
-// автоматическое определение типа всего объекта состояния
-export type RootState = ReturnType<typeof store.getState>
-// автоматическое определение типа метода dispatch
 export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 
-// для возможности обращения к store в консоли браузера
-// @ts-ignore
-window.store = store
